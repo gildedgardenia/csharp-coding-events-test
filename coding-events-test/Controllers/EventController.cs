a87cb6cacfdcf321be056a91c4d363ce1a2d39db
Sally Steuterman <sally.steuterman@gmail.com>
2020-04-02 20:27:28 -0500

    Initial view

---
+++ coding-events-test/Controllers/EventController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace coding_events_test.Controllers
+{
+    public class EventController : Controller
+    {
+        [HttpGet]
+        [Route("/events")]
+        public IActionResult Index()
+        {
+            List<string> events = new List<string>();
+
+            events.Add("Strange Loop");
+            events.Add("Apple WWDC");
+            events.Add("Code With Pride");
+
+            ViewBag.Events = events;
+
+            return View();
+        }
+    }
+}
---
+++ coding-events-test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coding_events_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
