9ae9a2cf398d9676e074a25c4617a75e4936e60f
Sally Steuterman <sally.steuterman@gmail.com>
2020-04-13 10:43:11 -0500

    Working on form without model

--- coding-events-test/Controllers/EventController.cs
+++ coding-events-test/Controllers/EventController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -22,6 +23,13 @@ namespace coding_events_test.Controllers
 
             ViewBag.Events = events;
 
+            return View();
+        }
+
+        [Route("/events/form")]
+        public IActionResult AddEventForm(string name)
+        {
+            ViewBag.Name = name;
             return View();
         }
     }
